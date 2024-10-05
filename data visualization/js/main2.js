2bf3d9685a51702e57c7a6a8a599c4a763fe86f6
MINJIE FU <minjief2@illinois.edu>
2024-10-05 15:08:00 -0600

    Add files via upload

---
+++ data visualization/js/main2.js
@@ -0,0 +1,111 @@
+const MARGIN = { LEFT: 120, RIGHT: 10, TOP: 100, BOTTOM: 100 }
+const WIDTH = 800 - MARGIN.LEFT - MARGIN.RIGHT
+const HEIGHT = 800 - MARGIN.TOP - MARGIN.BOTTOM
+
+
+let flag = true
+
+const svg = d3.select("#chart-area").append("svg")
+  .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+  .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+
+const g = svg.append("g")
+  .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
+
+// Title
+svg.append("text")
+   .attr("x", WIDTH / 2 + 130)
+   .attr("y", 50)
+   .attr("text-anchor", "middle")
+   .style("font-size", "25px")
+   .style("font-weight", "bold")
+   .text("Which dishes are most frequently appeared on the menus?");
+
+
+// Y label
+const yLabel = g.append("text")
+  .attr("class", "y axis-label")
+  .attr("x", - 200)
+  .attr("y", -60)
+  .attr("font-size", "20px")
+  .attr("text-anchor", "middle")
+  .attr("transform", "rotate(-90)")
+
+const x = d3.scaleBand()
+  .range([0, WIDTH])
+  .paddingInner(0.3)
+  .paddingOuter(0.2)
+
+const y = d3.scaleLinear()
+  .range([HEIGHT-150, 0])
+
+const xAxisGroup = g.append("g")
+  .attr("class", "x axis")
+  .attr("transform", `translate(0, 450)`)
+
+const yAxisGroup = g.append("g")
+  .attr("class", "y axis")
+
+d3.csv("data/revenues.csv").then(data => {
+  data.forEach(d => {
+    d.revenue = Number(d.revenue)
+    d.profit = Number(d.profit)
+  })
+
+  d3.interval(() => {
+    flag = !flag
+    
+    update(data)
+  }, 1500)
+
+  
+})
+
+function update(data) {
+  const value = flag ? "profit" : "revenue"
+  const t = d3.transition().duration(1000)
+
+  x.domain(data.map(d => d.month))
+  y.domain([0, d3.max(data, d => d[value])])
+
+  const xAxisCall = d3.axisBottom(x)
+  xAxisGroup.transition(t).call(xAxisCall)
+    .selectAll("text")
+      .attr("y", "10")
+      .attr("x", "-5")
+      .attr("text-anchor", "end")
+      .attr("transform", "rotate(-40)")
+
+  const yAxisCall = d3.axisLeft(y)
+    .ticks(5)
+    .tickFormat(d => d)
+  yAxisGroup.transition(t).call(yAxisCall)
+
+  // JOIN new data with old elements.
+  const rects = g.selectAll("rect")
+    .data(data, d => d.month)
+
+  // EXIT old elements not present in new data.
+  rects.exit()
+    .attr("fill", "red")
+    .transition(t)
+      .attr("height", 0)
+      .attr("y", y(0))
+      .remove()
+
+  // ENTER new elements present in new data...
+  rects.enter().append("rect")
+    .attr("fill", "grey")
+    .attr("y", y(0))
+    .attr("height", 0)
+    // AND UPDATE old elements present in new data.
+    .merge(rects)
+    .transition(t)
+      .attr("x", (d) => x(d.month))
+      .attr("width", x.bandwidth)
+      .attr("y", d => y(d[value]))
+      .attr("height", d => HEIGHT -150- y(d[value]))
+
+  const text = flag ? "Average Price($)" : "Times Appeared on the Menu"
+  yLabel.text(text)
+}
---
+++ data visualization/js/main3.js
@@ -0,0 +1,122 @@
+const MARGIN = { LEFT: 120, RIGHT: 10, TOP: 100, BOTTOM: 100 }
+const WIDTH = 800 - MARGIN.LEFT - MARGIN.RIGHT
+const HEIGHT = 800 - MARGIN.TOP - MARGIN.BOTTOM
+
+
+const svg = d3.select("#chart-area").append("svg")
+  .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+  .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+
+const g = svg.append("g")
+  .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
+
+
+
+// Title
+svg.append("text")
+   .attr("x", WIDTH / 2 + 130)
+   .attr("y", 50)
+   .attr("text-anchor", "middle")
+   .style("font-size", "25px")
+   .style("font-weight", "bold")
+   .text("How old are the collected menus?");
+
+
+// Y label
+g.append("text")
+  .attr("class", "y axis-label")
+  .attr("x", - 200)
+  .attr("y", -60)
+  .attr("font-size", "20px")
+  .attr("text-anchor", "middle")
+  .attr("transform", "rotate(-90)")
+  .text("Counts")
+
+
+  d3.csv("data/menus.csv").then(data => {
+    data.forEach(d => {
+      d.counts = Number(d.counts)
+      
+    })
+  
+
+    const x = d3.scaleBand()
+    .domain(data.map(d => d.year))
+    .range([0, WIDTH])
+    .paddingInner(0.3)
+    .paddingOuter(0.2)
+  
+  const y = d3.scaleLinear()
+    .domain([0, 1000])
+    .range([HEIGHT-150, 0])
+
+    const xAxisCall = d3.axisBottom(x)
+    .tickValues(x.domain().filter(function(d,i){ return !(i%10)}));
+   
+    
+    
+
+  g.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0, 450)")
+    .call(xAxisCall)
+    .selectAll("text")
+      .attr("y", "10")
+      .attr("x", "-5")
+      .attr("text-anchor", "end")
+      .attr("transform", "rotate(-40)")
+
+  const yAxisCall = d3.axisLeft(y)
+    .ticks(10)
+    .tickFormat(d => d)
+
+  g.append("g")
+    .attr("class", "y axis")
+    .call(yAxisCall)
+
+ var line = d3.line()   
+ .x(function(d) { return x(d.year)})   
+ .y(function(d) { return y(d.counts)})
+
+ g.append("path")
+ .datum(data)
+ .attr("fill", "none")
+ .attr("stroke", "steelblue")
+ .attr("stroke-linejoin", "round")
+ .attr("stroke-linecap", "round")
+ .attr("stroke-width", 2.5)
+ .attr("d", line)
+ .transition()
+ .duration(2500)
+ 
+ 
+ 
+ 
+  
+
+  });
+
+
+  const annotations = [
+    {
+      note: {
+        label: "70% of the menus in collections are dated within this period",
+        title: "1898-1907 Peak"
+      },
+      type: d3.annotationCalloutCircle,
+    subject: {
+      radius: 60,         // circle radius
+      radiusPadding: 0   // white space around circle befor connector
+    },
+    color: ["red"],
+    x: 228,
+    y: 100,
+    dy: 100,
+    dx: 100
+    }
+  ]
+  
+  // Add annotation to the chart
+  const makeAnnotations = d3.annotation()
+    .annotations(annotations)
+  g.call(makeAnnotations)
